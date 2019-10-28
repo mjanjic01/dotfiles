50de77efa17afcb9c472cef1bc2ef82c22f9a334
Matej Janjic <matej.janjic@productive.io>
2019-10-28 13:36:45 +0100

    Add readme and setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp .gitignore_global ~/.gitignore_global
+cp .gitconfig ~/.gitconfig
+
+cp .zshrc ~/.zshrc
+
+cp mjanjic.zsh-theme ~/.oh-my-zsh/themes/
+
+cp kitty.conf ~/.config/kitty/kitty.conf
+
+cp my.cnf /usr/local/etc/my.cnf
+
+cp -R ranger/ ~/.config/ranger/
