74128c0585004fad8169fa1a282432a2f9aaa59f
Matej Janjic <matej.janjic@productive.io>
2019-10-30 10:23:36 +0100

    Add brewfile, casklist and npmrc

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+brew bundle
+brew cask install $(cat files/Casklist)
+
 cp files/.gitignore_global ~/.gitignore_global
 cp files/.gitconfig ~/.gitconfig
 
 cp files/.zshrc ~/.zshrc
+
+cp files/.npmrc ~/.npmrc
 
 cp files/mjanjic.zsh-theme ~/.oh-my-zsh/themes/
 
