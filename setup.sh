d6530e5da0ae944b4c1e24bf802ce060bdb69702
Matej Janjic <matej.janjic@productive.io>
2019-10-30 10:27:26 +0100

    Format setup script

--- setup.sh
+++ setup.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
+# Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Install system dependencies
 brew bundle
 brew cask install $(cat files/Casklist)
 
+# Move configuration files
 cp files/.gitignore_global ~/.gitignore_global
 cp files/.gitconfig ~/.gitconfig
-
 cp files/.zshrc ~/.zshrc
-
 cp files/.npmrc ~/.npmrc
-
 cp files/mjanjic.zsh-theme ~/.oh-my-zsh/themes/
-
 cp files/kitty.conf ~/.config/kitty/kitty.conf
-
 cp files/my.cnf /usr/local/etc/my.cnf
-
 cp -R files/ranger/ ~/.config/ranger/
